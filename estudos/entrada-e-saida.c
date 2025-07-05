73dd3ca708959e47a4b84a0b9bd22f994544446c
João C. Santos <jcskater@gmail.com>
2025-07-05 22:45:15 +0000

    alteração de nome de arquivo, criação de código e executável do programa entrada-e-saida

---
+++ estudos/entrada-e-saida.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    float altura;
+    char opcao;
+    char nome[20];
+    
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+    printf("A idade digitada foi: %d\n", idade);
+
+    printf("Digite a sua altura: ");
+    scanf("%f", &altura);
+    printf("A altura digitada foi: %.2f\n", altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+    printf("Seu nome é: %s\n", nome);
+
+    printf("Digite sua opção: ");
+    scanf(" %c", &opcao);
+    printf("A opção digitada foi: %c\n", opcao);
+}

d1b2f43b6f0d31053137093b4043f5e11c277f24
João C. Santos <jcskater@gmail.com>
2025-07-05 21:16:59 +0000

    criando um arquivo com variáveis

---
+++ variaveis.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    //iniciando as variáveis
+    int idade;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = 'Sergio';
+
+    //declarando o valor da variável idade
+    idade = 25;
+
+    //alterando o valor da variável quantidade
+    quantidade = 10;
+}
